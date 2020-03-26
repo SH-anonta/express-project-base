53b52b755babf3a4d5e054a1f86b4f05f49a6af3
abcd <abcd@abcd.abcd>
2020-03-26 15:52:49 +0600

    Add configuration for enabling remote debuggers to work properly with typescript

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,22 @@
-import express from 'express';
-import dotEnv from 'dotenv';
-dotEnv.config();
+import 'source-map-support/register';
 
 import path from 'path';
+import dotEnv from 'dotenv';
+
+dotEnv.config();
 process.env.NODE_CONFIG_DIR = path.join(__dirname, './config');
+
+import express from 'express';
 import config from 'config';
 
 const webAppConfig = (config as any).webApp;
 const app = express();
 
 app.get('/', (req: any, res: any) => {
-  res.send('Hello World!');
+  console.log('ABC');
+  res.send('Za Warudo');
 });
 
 app.listen(webAppConfig.port, () => {
-  console.log(`App listening on port ${webAppConfig.port}.`)
+  console.log(`App listening on port ${webAppConfig.port}.`);
 });
