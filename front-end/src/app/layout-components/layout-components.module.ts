e659e98aaeb3b439b097dd002a2a20b9a320218d
abcd <abcd@abcd.abcd>
2020-04-11 16:03:44 +0600

    Add create user and edit user pages

--- front-end/src/app/layout-components/layout-components.module.ts
+++ front-end/src/app/layout-components/layout-components.module.ts
@@ -7,6 +7,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
@@ -21,6 +22,7 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    RouterModule,
   ],
   exports: [
     NavigationComponent
